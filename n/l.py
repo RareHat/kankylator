2016dc6c2d6de79e97c3bc1eb9fb42470282f32d
meimeuiii <152019926+meimeuiii@users.noreply.github.com>
2024-07-09 11:40:46 +0300

    kinech

--- n/l.py
+++ n/l.py
@@ -1,20 +1,24 @@
 from PyQt6.QtWidgets import*
 
 from res import app
-from themes import theme1, theme2
+from themes import theme1, theme2, theme3, theme4
 
 
 def settings():
     window = QDialog()
     main_line = QVBoxLayout()
     h1 = QHBoxLayout()
+    h2 = QHBoxLayout()
     br_btn = QPushButton("синьо-червона")
     pw_btn = QPushButton("рожево_біла")
     gp_btn = QPushButton("сіро-фіолетова")
+    st_btn = QPushButton("початкова тема")
     h1.addWidget(br_btn)
     h1.addWidget(pw_btn)
     h1.addWidget(gp_btn)
+    h2.addWidget(st_btn)
     main_line.addLayout(h1)
+    main_line.addLayout(h2)
 
     def change():
         app.setStyleSheet(theme1)
@@ -25,6 +29,15 @@ def settings():
         app.setStyleSheet(theme2)
 
     pw_btn.clicked.connect(change)
+
+    def change():
+        app.setStyleSheet(theme3)
+
+    gp_btn.clicked.connect(change)
+    def change():
+        app.setStyleSheet(theme4)
+
+    st_btn.clicked.connect(change)
     window.setLayout(main_line)
     window.show()
     window.exec()
--- themes.py
+++ themes.py
@@ -36,3 +36,49 @@ theme2 = ("""     QWidget {
         border-color: pink;        border-radius: 5px;
       }
  """)
+
+theme3 = ("""     QWidget {
+        background: #B266FF;     }
+     QPushButton {        background: #E0E0E0;
+        border-style: outset;        min-height: 30px;
+        min-width: 100px;     }
+     QListWidget {         background: #B266FF;
+     }     QTextEdit { 
+        background: #E0E0E0;     }
+      QPushButton {
+        color: black;        font-size: 15px;
+        font-family: Impact;        border-width: 2px;
+        border-color: purple;        border-radius: 5px;
+     }
+      QPushButton#save_btn {
+        color: black;        font-size: 20px;
+        font-family: Impact;        border-width: 4px;
+        border-color: purlpe;        border-radius: 5px;
+      }
+ """)
+
+theme4 = ("""     
+    QWidget {
+            background: black;    
+             }
+         QPushButton {        background: #e9edf2;
+            border-style: outset;        min-height: 30px;
+            min-width: 100px;     }
+         QListWidget {         background: black;
+         }     QTextEdit { 
+            background: #e1ede8;     }
+          QPushButton {
+            color: black;        font-size: 15px;
+            font-family: Impact;        border-width: 2px;
+            border-color: gray;        border-radius: 5px;
+         }
+          QPushButton#save_btn {
+            color: green;        font-size: 20px;
+            font-family: Impact;        border-width: 4px;
+            border-color: grey;        border-radius: 5px;
+          }
+          QLineEdit {
+            background-color: white;   
+    
+          }
+     """)
